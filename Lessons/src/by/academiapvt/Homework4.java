c461a6a3d5a21d3d8b4f61c8a8de2ad9a484b544
pprof <novik_vladimir@tut.by>
2023-03-17 17:53:23 +0300

    one

--- Lessons/src/by/academiapvt/Homework4.java
+++ Lessons/src/by/academiapvt/Homework4.java
@@ -25,15 +25,44 @@ public class Homework4 {
             }
             System.out.println("Сумма чисел между заданными числами равна " + c);
         }
-        if (a < b) {
+        else if (a < b) {
             for (n = (b - 1); n > a; n--) {
                 c += n;
             }
             System.out.println("Сумма чисел между заданными числами равна " + c);
         }
-        if (a == b) System.out.println("Вы ввели одинаковые числа");
+        else System.out.println("Вы ввели одинаковые числа");
 
-//        ************** Task3
+//        ************ Task3
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число, не кратное 10");
+        int k = scanner.nextInt();
+        int m = 0;
+        while (k > 0) {
+            m = k % 10;
+            k /= 10;
+            System.out.print(m);
+        }
+
+//        ************ Task4
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число, не кратное 10");
+        int z = scanner.nextInt();
+        int x = 0;
+        int y;
+        System.out.println("Четными цифрами во введенном числе являются: ");
+        y=z;
+        while (y > 0) {
+            x = y % 10;
+            if ((y%2)==0) System.out.print(x);
+            y /= 10;
+        }
+        System.out.println("\n" + "Нечетными цифрами во введенном числе являются: ");
+        while (z > 0) {
+            x = z % 10;
+            if ((z%2)>0) System.out.print(x);
+            z /= 10;
+        }
 
     }
 }
