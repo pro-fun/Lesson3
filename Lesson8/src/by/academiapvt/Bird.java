1ac422624a6a37146e00ae11e3e0522f73d50bbd
pprof <novik_vladimir@tut.by>
2023-04-03 18:22:51 +0300

    дз зоопарк без абстракции

---
+++ Lesson8/src/by/academiapvt/Bird.java
@@ -0,0 +1,47 @@
+package by.academiapvt;
+
+public class Bird extends Animal {
+    String family;
+    String flyHeight;
+
+
+    public Bird(String color, int lifeLongMax, String foodType, String family, String flyHeight) {
+        super(color, lifeLongMax, foodType);
+        this.family = family;
+        this.flyHeight = flyHeight;
+    }
+
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getFlyHeight() {
+        return flyHeight;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public void setFlyHeight(String flyHeight) {
+        this.flyHeight = flyHeight;
+    }
+
+    protected void toSing() {
+        System.out.println(family + " поет");
+    }
+
+    protected void toPeck() {
+        System.out.println(family + " клюет");
+    }
+
+    protected void toHatchChicks() {
+        System.out.println(family + " высиживает птенцов");
+    }
+
+    public String toString() {
+        return super.toString() + " , семейство - " + family + " , высота полета - " + flyHeight;
+
+    }
+}
